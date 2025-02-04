027ca3c89865cea7e98dc45f76b54faafc67fe94
gus2217 <kiptoocarroll@gmail.com>
2025-02-04 20:18:45 +0300

    hostel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HostelComponent } from './features/components/hostel/hostel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin/hostels',component:HostelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { HostelComponent } from './features/components/hostel/hostel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HostelComponent,
   ],
   imports: [
     BrowserModule,
